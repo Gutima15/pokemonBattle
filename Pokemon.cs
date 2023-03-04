0b60226b1776d6426bcec9781e6020804504b2c1
Jorge Gutiérrez <jorgeedugutima15@gmail.com>
2023-03-03 22:09:35 -0600

    Adding the pokemon class and Scratch attack

---
+++ Pokemon.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Pokemon {
+    private string name;
+    private int hp;
+    private int defense;
+
+    public getHp(){
+        return this.hp;
+    }
+
+    public getName(){
+        return this.name
+    }
+    
+    public getDefense(){
+        return this.defense
+    }
+
+
+    public setName(){
+        
+    }
+
+    public setHp(){
+        
+    }
+}
