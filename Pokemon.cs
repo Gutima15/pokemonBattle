cd6d27428ff67d4cd15cd920c6f00ca86f975a1d
Jorge Gutiérrez <jorgeedugutima15@gmail.com>
2023-03-03 22:20:36 -0600

    adding pokemon class changes

--- Pokemon.cs
+++ Pokemon.cs
@@ -1,28 +1,27 @@
 using System;
 
 class Pokemon {
-    private string name;
-    private int hp;
-    private int defense;
+    public string name;
+    public int hp;
+    public int defense;
+    public int attack;
+    public int evasion;
 
-    public getHp(){
-        return this.hp;
+    public Pokemon(string name, int hp, int attack, int defense, int evasion){
+        this.name = name;
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.evasion = evasion;
+    }
+    public void Scratch(Pokemon enemy){
+        int damage = (this.attack - enemy.defense) * 5;
+        Random random = new Random();
+        int evasionSelector = random.Next(1,4);
+        if(evasionSelector > 3) {
+            enemy.hp = enemy.hp - this.damage;
+            console.WriteLine($"{this.name} use Scratch! It makes {damage} damage to {enemy.name}.);
+        }
     }
 
-    public getName(){
-        return this.name
-    }
-    
-    public getDefense(){
-        return this.defense
-    }
-
-
-    public setName(){
-        
-    }
-
-    public setHp(){
-        
-    }
 }
--- Program.cs
+++ Program.cs
@@ -1,33 +1,8 @@
 ﻿using System;
 
-namespace pokemonBattle
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //testing access to variable definitions
-            int explicitVariable = 10;
-            Console.WriteLine(explicitVariable.GetType()); // prints System.String
-            var implicitVariable = 20;
-            // Console.WriteLine(implicitVariable.GetType()); // prints System.String
-            // implicitVariable = "Carlitos";
-            // Console.WriteLine(implicitVariable.GetType()); // prints System.String
-
-            //Math
-            int firstValue = 10, secondValue = 5;                       
-            double pi = 3.14;
-            Console.WriteLine("addition: " + firstValue + secondValue);
-
-            Console.WriteLine("Substraction: " + (firstValue - secondValue));
-
-            Console.WriteLine("product: " + firstValue * secondValue);
-
-            Console.WriteLine("divide: " + firstValue / secondValue);
-
-            Console.WriteLine($"Module {firstValue}" + firstValue % secondValue);
-
-            Console.WriteLine(256*2);
+namespace pokemonBattle {
+    class Program {
+        static void Main(string[] args) {
 
         }
     }
